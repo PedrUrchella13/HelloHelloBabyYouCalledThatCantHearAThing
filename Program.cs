08a29be4f14532c075b12583bb94d4016bdd15b9
PedrUrchella13 <pedrourchella@gmail.com>
2025-07-31 10:32:56 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var numero1 = 2;
+        var numero2 = 3;
+
+        var soma = numero1 + numero2;
+
+        Console.WriteLine("I won't cry for you");
+        Console.WriteLine("I won't crucify the things you do");
+
+        Console.WriteLine($"The sum of {numero1} and {numero2} is {soma}");
+    }
+}
